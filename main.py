063aaadb1e544ae919773d7431a1badaa65d85ca
rcasalins <92891280+rcasalins@users.noreply.github.com>
2023-02-11 20:50:55 -0500

    Ultimate

--- main.py
+++ main.py
@@ -1,7 +1,10 @@
 #from typing import Union
-from fastapi import FastAPI, Query
+import json
+from fastapi import FastAPI, Query, Path
 from starlette.requests import Request
 from fastapi.responses import JSONResponse
+from pydantic import BaseModel
+from fastapi.encoders import jsonable_encoder
 
 from fastapi import FastAPI
 
@@ -15,12 +18,36 @@ async def user_agent(request: Request, call_next):
     else:
         return JSONResponse(content = {'message': 'Do not use a fucking mobile'}, status_code = 401)
 
-# class Item(BaseModel):
-#     name: str
-#     description: str | None = None
-#     price: float
-#     tax: float | None = None
+DB = {}
 
-@app.get('/')
+@app.post('/')
 def root():
-    return {'message': 'Hello World'}
+    return DB
+
+class User(BaseModel):
+    password: str
+
+@app.put('/{user_id}')
+async def user_put(user_id: str, passw: User):
+    json_compatible_item_data = jsonable_encoder(passw)
+    DB[user_id] = json_compatible_item_data
+    return DB
+
+@app.get('/verification/{user}')
+async def user_validation(user: str, password: str):
+    result = 'Active'
+    if DB.get(user):
+        if DB[user] == {'password': password}:
+
+            return result
+
+    return JSONResponse(content = {'message': 'User or password wrong'}, status_code = 401)
+
+@app.delete('/delete/{user}')
+async def user_delete(user: str):
+    if DB.get(user):
+        del DB[user]
+    else:
+        return JSONResponse(content = {'message': 'User or password wrong'}, status_code = 401)
+
+    return DB
